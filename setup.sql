518b624f48e0fe4450d45e4fefa39d938ec26fb9
bmackenty <bmackenty@gmail.com>
2017-02-17 23:08:02 +0100

    This updates the SQL changes

--- setup.sql
+++ setup.sql
@@ -17,7 +17,7 @@ CREATE TABLE `userorganization` (
   `country` VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `userrole` (
+CREATE TABLE `membership` (
   `id` INTEGER PRIMARY KEY AUTO_INCREMENT,
   `user_group` INTEGER NOT NULL,
   `user` INTEGER NOT NULL
@@ -27,6 +27,6 @@ CREATE INDEX `idx_userrole__user` ON `userrole` (`user`);
 
 CREATE INDEX `idx_userrole__user_group` ON `userrole` (`user_group`);
 
-ALTER TABLE `userrole` ADD CONSTRAINT `fk_userrole__user` FOREIGN KEY (`user`) REFERENCES `user` (`id`);
+ALTER TABLE `membership` ADD CONSTRAINT `fk_userrole__user` FOREIGN KEY (`user`) REFERENCES `user` (`id`);
 
-ALTER TABLE `userrole` ADD CONSTRAINT `fk_userrole__user_group` FOREIGN KEY (`user_group`) REFERENCES `usergroup` (`id`);
+ALTER TABLE `membership` ADD CONSTRAINT `fk_userrole__user_group` FOREIGN KEY (`user_group`) REFERENCES `usergroup` (`id`);
