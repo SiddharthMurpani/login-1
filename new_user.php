86addd591acfedd54b764c323847d2ec6afe9b37
Bill MacKenty <bmackenty@users.noreply.github.com>
2017-03-01 05:40:17 +0100

    Update new_user.php
    cleaned up

--- new_user.php
+++ new_user.php
@@ -2,41 +2,29 @@
 
 // create a connection
 
-$connect = mysqli_connect("localhost","bill","pa55word!","bill");
-
-// Check connection
-
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    die;
-}
+include('database_connect.php');
 
 $new_user_email = $_POST['new_user_email'];
 $new_user_password = $_POST['new_user_password'];
 
 // Perform queries
+// we always sanitize user input. I haven't taught you this yet. 
+// Including a variable in SQL query is generlly a bad idea. Prepared statements are much healthier. 
 
 $result = mysqli_query($connect,"SELECT * FROM user WHERE email LIKE '$new_user_email'");
 
 if(mysqli_num_rows($result) == 0){
 
+ // this is also where we would hash and slat the users password. NEVER STORE PLANTEXT PASSWORDS IN A DATABASE!    
     echo "looks like a unique email address";
     $insert_new_user= mysqli_query($connect,"INSERT INTO user (email,password) VALUES ('$new_user_email','$new_user_password')");
 
 
 } else {
 
-    echo "looks like a duplicate email address";
+    echo "Error: looks like a duplicate email address";
     die;
 
 }
 
-
-/**
- * Created by PhpStorm.
- * User: bmackenty
- * Date: 28/02/17
- * Time: 12:46
- */
 ?>
