e38102d805ea598cccc5425e1705516e3f7fdced
Bill MacKenty <bmackenty@users.noreply.github.com>
2017-03-01 05:36:27 +0100

    Update connect.php
    cleaned up a bit

--- connect.php
+++ connect.php
@@ -2,15 +2,7 @@
 
 // create a connection
 
-$connect = mysqli_connect("localhost","bill","pa55word!","bill");
-
-// Check connection
-
-if (mysqli_connect_errno())
-{
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    die;
-}
+include('database_connection.sql');
 
 // Perform queries
 
