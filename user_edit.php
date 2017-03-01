bbfdd9d006940cd94bed585a2d7722d8bc642ff9
Bill MacKenty <bmackenty@users.noreply.github.com>
2017-03-01 05:44:51 +0100

    Update user_edit.php

--- user_edit.php
+++ user_edit.php
@@ -1,5 +1,8 @@
 <?php
 
+// this page can only be called from a trusted page, but we should include protections so only 
+// users in a certain group can call it. Also, what would happen (weird) if a user ID was claaed that wasnt in our database?
+
 $user_id = $_GET['id'];
 echo "user id is $user_id";
 
