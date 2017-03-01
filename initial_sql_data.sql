b8368c22928a5216556134dd4d153c907fc132b5
Bill MacKenty <bmackenty@users.noreply.github.com>
2017-03-01 05:28:44 +0100

    Update initial_sql_data.sql

--- initial_sql_data.sql
+++ initial_sql_data.sql
@@ -1,10 +1,10 @@
 -- noinspection SqlDialectInspectionForFile
 
-use bill;
+use YOUR DATABASE HERE;
 
-INSERT INTO user(email,password) VALUES ('bill@mackenty.org','12345');
-INSERT INTO user(email,password) VALUES ('bmackenty@aswarsaw.org','12345');
-INSERT INTO user(email,password) VALUES ('bmackenty@gmail.com','54321');
+INSERT INTO user(email,password) VALUES ('user01@myemail.com','12345');
+INSERT INTO user(email,password) VALUES ('user02@myemail.com','12345');
+INSERT INTO user(email,password) VALUES ('user03@myNEWemail.com','54321');
 
 INSERT INTO usergroup(name,description) VALUES ('member','This is default group for members. Able to access the site, and see private content, but not able to see admin functions.');
 INSERT INTO usergroup(name,description) VALUES ('administrator','This is the group for administrators.');
